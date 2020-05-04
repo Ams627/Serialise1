1e6f44947c9637e08e03dcb325a833b9d6fc3015
Adrian Sims <adriansims62781@gmail.com>
2020-05-04 17:34:15 +0200

    Add project files.

---
+++ Serialise1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Serialise1
+{
+    [Serializable]
+    public class X
+    {
+        private string _xml;
+        private XElement _xElement;
+
+        public string Xml
+        {
+            get
+            {
+                return _xml;
+            }
+            set
+            {
+                _xml = value;
+                _xElement = null;
+            }
+        }
+
+//        [XmlIgnore]
+        public XElement XElement
+        {
+            get => GetXElement();
+            set
+            {
+                _xElement = value;
+            }
+        }
+
+        private XElement GetXElement()
+        {
+            _xElement = XElement.Parse(_xml);
+            return _xElement;
+        }
+
+        public X() { }
+        public X(string xml)
+        {
+            _xml = xml;
+        }
+    }
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var xml = "<Root></Root>";
+                var serialiser = new XmlSerializer(typeof(X));
+                var x = new X(xml);
+
+                Console.WriteLine($"{x.XElement}");
+
+                var stringwriter = new StringWriter();
+
+                serialiser.Serialize(stringwriter, x);
+                Console.WriteLine(stringwriter.ToString());
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
